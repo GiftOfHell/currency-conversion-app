c5eb7ce7d426e7a7fa7d637037708a4db67d665a
Владислав Бурачёнок <vlad.burachonok@gmail.com>
2024-04-23 20:38:05 +0300

    Add new user registration. Add registration validation.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { NgIf } from '@angular/common';
+import { passwordMatchValidator } from '../../validators/password-match.validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  registrationForm!: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(4)]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: passwordMatchValidator }
+    );
+  }
+
+  get username() {
+    return this.registrationForm.controls['username'];
+  }
+
+  get password() {
+    return this.registrationForm.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.registrationForm.controls['confirmPassword'];
+  }
+
+  registerUser() {
+    console.log(this.username);
+    this.submitted = true;
+    if (this.registrationForm.invalid) {
+      return;
+    }
+
+    const username = this.registrationForm.value.username;
+    const password = this.registrationForm.value.password;
+
+    const isUsernameTaken = this.userService.isUsernameTaken(username);
+    if (isUsernameTaken) {
+      this.username.setErrors({ taken: true });
+    } else {
+      this.userService.createUser(username, password);
+
+      this.registrationForm.reset();
+      this.router.navigate(['login']);
+    }
+  }
+}
